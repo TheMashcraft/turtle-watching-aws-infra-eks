881caa3fd72ab6e4a896bf1a1ebcd9d5d5f1584f
Matt Ashcraft <ashcraft39@gmail.com>
2024-07-01 23:35:26 -0400

    Remove laod config file argument

--- provider.tf
+++ provider.tf
@@ -6,5 +6,4 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.main.endpoint
   token                  = data.aws_eks_cluster_auth.main.token
   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-  load_config_file       = false
 }
